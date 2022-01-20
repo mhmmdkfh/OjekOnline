c98af1f4912c5c2d658b0c6539d0cc99864d7960
mhmmdkfh <muhammad.kifahi95@gmail.com>
2022-01-20 18:29:59 +0700

    Create view profile and view wallet driver

---
+++ DriverService/Dtos/DriverDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DriverService.Dtos
+{
+    public class DriverDto
+    {
+        public int NIK { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}
---
+++ DriverService/Dtos/WalletDriverDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DriverService.Dtos
+{
+    public class WalletDriverDto
+    {
+        public string Email { get; set; }
+        public float Wallet { get; set; }
+    }
+}
