7059ff1998918a382b0427bfe05e56cf3a3e1a40
Gilang Pratama Wiguna <gilangpw84@gmail.com>
2022-01-22 18:04:30 +0700

    up

--- CustomerService/Migrations/20220111134820_InitTable.cs
+++ CustomerService/Migrations/20220111134820_InitTable.cs
@@ -22,6 +22,25 @@ namespace CustomerService.Migrations
                 {
                     table.PrimaryKey("PK_Customers", x => x.Id);
                 });
+
+            migrationBuilder.CreateTable(
+            name: "Orders",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                DriverId = table.Column<int>(type: "int", nullable: false),
+                CustomerId = table.Column<int>(type: "int", nullable: false),
+                Lat = table.Column<double>(type: "float", nullable: false),
+                Long = table.Column<double>(type: "float", nullable: false),
+                Price = table.Column<float>(type: "real", nullable: false),
+                IsAccepted = table.Column<bool>(type: "bit", nullable: false),
+                IsFinished = table.Column<bool>(type: "bit", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Orders", x => x.Id);
+            });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
