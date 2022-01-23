ce30da7ca7020df90ebaa1ab3170cb82b8347350
karifhar <rifqahanif.a@gmail.com>
2022-01-23 14:02:20 +0700

    fix route

--- AdminService/Controllers/AdminController.cs
+++ AdminService/Controllers/AdminController.cs
@@ -158,7 +158,7 @@ namespace AdminService.Controllers
             }
         }
 
-        [HttpPut]
+        [HttpGet("Price")]
         public async Task<Rate> GetPrice()
         {
             var setPrice = await _admin.GetPrice();
